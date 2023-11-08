c53bcbfbc43bcadfebcea82c3d88dc960785f297
Neo Rosenlund <Neo.rosenlund@elev.ga.ntig.se>
2023-11-08 09:32:43 +0100

    EnemyFaces Player when he sees him

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -8,18 +9,30 @@ public class Enemy : MonoBehaviour
 [SerializeField]Transform eyes;
 bool seesPlayer;
 bool shoot;
-[SerializeField] LayerMask player;
+Vector2 FaceDir;
+[SerializeField] LayerMask playerLayer;
+[SerializeField] Transform PlayerTr;
+[SerializeField] LayerMask EnemyLayer;
 
 
 
-
-
+    
  
 
     void Update()
     {
-        
+        FaceDir = transform.position - PlayerTr.transform.position;
+        SeePlayer(eyes.position, PlayerTr.position);
 
+        if(seesPlayer == true){
+            
+            if(PlayerTr.position.x > transform.position.x){
+                transform.right = new Vector3(1,0,0);
+            }else if(PlayerTr.position.x < transform.position.x){
+                transform.right = new Vector3(-1,0,0);
+            }
+
+        }
 
 
         
@@ -28,10 +41,14 @@ bool shoot;
 
    public void SeePlayer(Vector3 eyePos, Vector3 playerPos){
         
-        RaycastHit2D hitInfo = Physics2D.Raycast(eyePos, playerPos);
-        
+
+
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(eyePos, FaceDir, ~EnemyLayer);
         if(hitInfo.collider != null){
-            if(hitInfo.transform.gameObject.layer == player){
+            Debug.Log(hitInfo.transform.name);
+
+            if(hitInfo.transform.gameObject.name == "Player"){
                 seesPlayer = true;
             }else{
                 seesPlayer = false;
@@ -42,6 +59,11 @@ bool shoot;
 
     }
 
+         private void OnDrawGizmos() {
+
+                Gizmos.color = Color.red;
+            Gizmos.DrawLine(eyes.position, PlayerTr.position);
+        }
 
 
 
